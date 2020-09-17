b890e1bee959dd780f486f69d18733bc0de7e2c3
johncoryk <coryk0789@gmail.com>
2020-09-17 11:39:00 -0400

    set up controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,29 @@
 class CharactersController < ApplicationController
+  def index
+    characters = Character.all
+    render json: { characters: characters}
+  end
+
+  def show
+    characters = Character.find(params[:id])
+    render json: { character: character}
+  end
+
+  def create
+    characters = Character.create!(character_params)
+    render json: { character: character}
+  end
+
+  def update
+  end
+
+  def destroy
+    character = Character.find(params[:id])
+    character.destroy
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :game)
+  end
 end
